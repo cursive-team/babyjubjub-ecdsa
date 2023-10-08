63abebcedab371b53abb74df81b15e4002a71f75
AndrewCLu <andrewclu98@gmail.com>
2023-10-07 21:55:29 -0400

    add tests for ecdsa functions, batch proving, utils

--- packages/lib/test/ecdsa.test.ts
+++ packages/lib/test/ecdsa.test.ts
@@ -1,3 +1,4 @@
+import { WeierstrassPoint } from "../src/babyJubjub";
 import {
   privateKeyToPublicKey,
   recoverPubKeyIndexFromSignature,
@@ -90,5 +91,123 @@ describe("ecdsa public key recovery", () => {
     const expectedIndex = 2;
 
     expect(recoveredIndex).toEqual(expectedIndex);
+  });
+
+  it("should error if the public key is not in the list", () => {
+    const pubKeys = [
+      "041941f5abe4f903af965d707182b688bd1fa725fd2cbc648fc435feb42a3794593275a2e9b4ad4bc0d2f3ecc8d23e3cf89da889d7aa35ce33f132d87b5bb5c393",
+      "049ae9f2ec6a4db43f0e081a436f885b0d3f5753a45b00d2f2e3da38956848c4ff0205d89e14a2e36976bfe033407dbce6b48261d84d201277de0c3b82f08ddb09",
+      "044d9d03f3266f24777ac488f04ec579e1c4bea984398c9b98d99a9e31bc75ef0f13a19471a7297a6f2bf0126ed93d4c55b6e98ec286203e3d761c61922e3a4cda",
+      "044d9d03f3266f24777ac488f04ec579e1c4bea984398c9b98d99a9e31bc75ef0f13a19471a7297a6f2bf0126ed93d4c55b6e98ec286203e3d761c61922e3a4cda",
+    ];
+    const msgHash = BigInt("0");
+    const sig = {
+      r: hexToBigInt(
+        "00EF7145470CEC0B683C629CBA8ED58110000FFE657366F7D5A91F2D149DD8B5"
+      ),
+      s: hexToBigInt(
+        "0370C60A23266F520C56DA088B4C4AFAAAF6BB1993A501980F6D8FB6F343984A"
+      ),
+    };
+
+    const pubKeyPoints = pubKeys.map((pubKey) => publicKeyFromString(pubKey));
+
+    expect(() =>
+      recoverPubKeyIndexFromSignature(sig, msgHash, pubKeyPoints)
+    ).toThrow();
+  });
+
+  it("should error if public key is not recoverable", () => {
+    const pubKeys = [
+      "041941f5abe4f903af965d707182b688bd1fa725fd2cbc648fc435feb42a3794593275a2e9b4ad4bc0d2f3ecc8d23e3cf89da889d7aa35ce33f132d87b5bb5c393",
+      "049ae9f2ec6a4db43f0e081a436f885b0d3f5753a45b00d2f2e3da38956848c4ff0205d89e14a2e36976bfe033407dbce6b48261d84d201277de0c3b82f08ddb09",
+      "041052d6da0c3d7248e39e08912e2daa53c4e54cd9f2d96e3702fa15e77b199a501cd835bbddcc77134dc59dbbde2aa702183a68c90877906a31536eef972fac36",
+      "044d9d03f3266f24777ac488f04ec579e1c4bea984398c9b98d99a9e31bc75ef0f13a19471a7297a6f2bf0126ed93d4c55b6e98ec286203e3d761c61922e3a4cda",
+    ];
+    const msgHash = BigInt("0");
+    const sig = {
+      r: hexToBigInt(
+        "00EF7145470CEC0B683C629CBA8ED58110000FFE657366F7D5A91F2D149DD8B4"
+      ),
+      s: hexToBigInt(
+        "0370C60A23266F520C56DA088B4C4AFAAAF6BB1993A501980F6D8FB6F343984A"
+      ),
+    };
+
+    const pubKeyPoints = pubKeys.map((pubKey) => publicKeyFromString(pubKey));
+
+    expect(() =>
+      recoverPubKeyIndexFromSignature(sig, msgHash, pubKeyPoints)
+    ).toThrow();
+  });
+});
+
+describe("private to public key conversion", () => {
+  test("should convert a private key to a public key 0", () => {
+    const privKey =
+      "0323dbbda9a5aff570d974d71c88334cf99ab9c0455e1d2546ca03ca069eb1e0";
+    const pubKey = privateKeyToPublicKey(hexToBigInt(privKey));
+
+    const expectedPubKey = new WeierstrassPoint(
+      BigInt(
+        "7383369888919701441480368741745717804236448589785295824485316386504973064784"
+      ),
+      BigInt(
+        "13046769583748125084667126323794391074141340611556711664428099286902963678262"
+      )
+    );
+
+    expect(pubKey).toEqual(expectedPubKey);
+  });
+
+  test("should convert a private key to a public key 1", () => {
+    const privKey =
+      "04b81e7180cd9504ce1bf0f728b4c828ad369781986aff07284d60ec1d59850b";
+    const pubKey = privateKeyToPublicKey(hexToBigInt(privKey));
+
+    const expectedPubKey = new WeierstrassPoint(
+      BigInt(
+        "8022836036792728510020073593790625374591718668941094754729876067757770684825"
+      ),
+      BigInt(
+        "2888043282838146526405156459340757249954671046314727413645487686643973993965"
+      )
+    );
+
+    expect(pubKey).toEqual(expectedPubKey);
+  });
+
+  test("should convert a private key to a public key 2", () => {
+    const privKey =
+      "02ea6ba4d6ec9b1b724f93a5ddf4ddcc94fc09909753088c272970fe3c99c4d8";
+    const pubKey = privateKeyToPublicKey(hexToBigInt(privKey));
+
+    const expectedPubKey = new WeierstrassPoint(
+      BigInt(
+        "9714024128310316092057230443118059995407288196097449837364797009977227758081"
+      ),
+      BigInt(
+        "3986000760809817233522229217388333078261970153215912556094459527204210942908"
+      )
+    );
+
+    expect(pubKey).toEqual(expectedPubKey);
+  });
+
+  test("should convert a private key to a public key 3", () => {
+    const privKey =
+      "0323dbbda9a5aff570d974d71c88334cf99ab9c0455e1d2546ca03ca069eb1e0";
+    const pubKey = privateKeyToPublicKey(hexToBigInt(privKey));
+
+    const expectedPubKey = new WeierstrassPoint(
+      BigInt(
+        "7383369888919701441480368741745717804236448589785295824485316386504973064784"
+      ),
+      BigInt(
+        "13046769583748125084667126323794391074141340611556711664428099286902963678262"
+      )
+    );
+
+    expect(pubKey).toEqual(expectedPubKey);
   });
 });
--- packages/lib/test/utils.test.ts
+++ packages/lib/test/utils.test.ts
@@ -1,10 +1,71 @@
-import { hashEdwardsPublicKey, publicKeyFromString } from "../src/utils";
+import {
+  bigIntToBytes,
+  bigIntToHex,
+  bytesToBigInt,
+  bytesToHex,
+  derDecode,
+  hashEdwardsPublicKey,
+  hexToBigInt,
+  hexToBytes,
+  publicKeyFromString,
+} from "../src/utils";
 import { EdwardsPoint, WeierstrassPoint } from "../src/babyJubjub";
 // @ts-ignore
 import { buildPoseidonReference } from "circomlibjs";
 
 describe("signature and key parsing utilities", () => {
-  test("should parse an encoded public key", () => {
+  test("should DER decode a signature 0", () => {
+    const encodedSig =
+      "30440220036E3AD3E9358B8299A60150BB925DEF60519861DB29E6468366ABE441F04C71022003872AABF9BE3935EF255FDB847A09E1789990BE85C3C368589D7693D0E5B36F";
+
+    const expectedr = hexToBigInt(
+      "036E3AD3E9358B8299A60150BB925DEF60519861DB29E6468366ABE441F04C71"
+    );
+    const expecteds = hexToBigInt(
+      "03872AABF9BE3935EF255FDB847A09E1789990BE85C3C368589D7693D0E5B36F"
+    );
+
+    const parsedSig = derDecode(encodedSig);
+
+    expect(parsedSig.r).toEqual(expectedr);
+    expect(parsedSig.s).toEqual(expecteds);
+  });
+
+  test("should DER decode a signature 1", () => {
+    const encodedSig =
+      "3044022001E82E797E53FB528D707B20513FC1B181A16315390DFC57FFCB477AC24A375E022004F7B2BCA543DEC95D6F82BC355C8E99F34DA07DE229B3A5D32999AB515F18E8";
+
+    const expectedr = hexToBigInt(
+      "01E82E797E53FB528D707B20513FC1B181A16315390DFC57FFCB477AC24A375E"
+    );
+    const expecteds = hexToBigInt(
+      "04F7B2BCA543DEC95D6F82BC355C8E99F34DA07DE229B3A5D32999AB515F18E8"
+    );
+
+    const parsedSig = derDecode(encodedSig);
+
+    expect(parsedSig.r).toEqual(expectedr);
+    expect(parsedSig.s).toEqual(expecteds);
+  });
+
+  test("should DER decode a signature 2", () => {
+    const encodedSig =
+      "30440220050AFA65DFD6E8709364DCF739FBAF2D6B436F84ADD5296BEE38BC65FA116912022001E8390CB9EF3688E2F319C0D08BB5DC11442BA9A93453660CD86B3728D0C106";
+
+    const expectedr = hexToBigInt(
+      "050AFA65DFD6E8709364DCF739FBAF2D6B436F84ADD5296BEE38BC65FA116912"
+    );
+    const expecteds = hexToBigInt(
+      "01E8390CB9EF3688E2F319C0D08BB5DC11442BA9A93453660CD86B3728D0C106"
+    );
+
+    const parsedSig = derDecode(encodedSig);
+
+    expect(parsedSig.r).toEqual(expectedr);
+    expect(parsedSig.s).toEqual(expecteds);
+  });
+
+  test("should parse an encoded public key 0", () => {
     const encodedPubKey =
       "041052d6da0c3d7248e39e08912e2daa53c4e54cd9f2d96e3702fa15e77b199a501cd835bbddcc77134dc59dbbde2aa702183a68c90877906a31536eef972fac36";
 
@@ -16,6 +77,24 @@ describe("signature and key parsing utilities", () => {
       ),
       BigInt(
         "13046769583748125084667126323794391074141340611556711664428099286902963678262"
+      )
+    );
+
+    expect(parsedPubKey).toEqual(expectedPubKey);
+  });
+
+  test("should parse an encoded public key 1", () => {
+    const encodedPubKey =
+      "0411bcc3a7bc7d083b6c67fc7fd33a31bafdfcbf8883dbbf1ab6fc3eba321c39990662931714e04b3a3deb0c6102d9bf7a7ac56ba7d281afc07afa803e65d9b5ed";
+
+    const parsedPubKey = publicKeyFromString(encodedPubKey);
+
+    const expectedPubKey = new WeierstrassPoint(
+      BigInt(
+        "8022836036792728510020073593790625374591718668941094754729876067757770684825"
+      ),
+      BigInt(
+        "2888043282838146526405156459340757249954671046314727413645487686643973993965"
       )
     );
 
@@ -41,7 +120,7 @@ describe("signature and key parsing utilities", () => {
     );
   });
 
-  test("poseidon reference is correct", async () => {
+  test("poseidon reference is correct 0", async () => {
     const expected =
       "115cc0f5e7d690413df64c6b9662e9cf2a3617f2743245519e19607a4417189a";
     const a = 1;
@@ -53,7 +132,7 @@ describe("signature and key parsing utilities", () => {
     expect(hash).toEqual(expected);
   });
 
-  test("poseidon reference is correct 2", async () => {
+  test("poseidon reference is correct 1", async () => {
     const expected =
       "473788188026338532754827614266124932928610354793582188635738505121763471517";
     const a =
@@ -65,5 +144,51 @@ describe("signature and key parsing utilities", () => {
     const hash = poseidon.F.toString(poseidon([a, b]), 10);
 
     expect(hash).toEqual(expected);
+  });
+
+  test("converts hex to bigint correctly", () => {
+    const hex = "abcabcabcabc123456";
+    const expected = BigInt("3169001976782843425878");
+
+    expect(hexToBigInt(hex)).toEqual(expected);
+  });
+
+  test("converts bigint to hex correctly", () => {
+    const bigInt = BigInt("3169001976782843425878");
+    const expected = "abcabcabcabc123456";
+
+    expect(bigIntToHex(bigInt)).toEqual(expected);
+  });
+
+  test("converts hex to bytes correctly", () => {
+    const hex = "abcabcabcabc123456";
+    const expected = Uint8Array.from([
+      171, 202, 188, 171, 202, 188, 18, 52, 86,
+    ]);
+
+    expect(hexToBytes(hex)).toEqual(expected);
+  });
+
+  test("converts bytes to hex correctly", () => {
+    const bytes = Uint8Array.from([171, 202, 188, 171, 202, 188, 18, 52, 86]);
+    const expected = "abcabcabcabc123456";
+
+    expect(bytesToHex(bytes)).toEqual(expected);
+  });
+
+  test("converts bytes to bigint correctly", () => {
+    const bytes = Uint8Array.from([171, 202, 188, 171, 202, 188, 18, 52, 86]);
+    const expected = BigInt("3169001976782843425878");
+
+    expect(bytesToBigInt(bytes)).toEqual(expected);
+  });
+
+  test("converts bigint to bytes correctly", () => {
+    const bigInt = BigInt("3169001976782843425878");
+    const expected = Uint8Array.from([
+      171, 202, 188, 171, 202, 188, 18, 52, 86,
+    ]);
+
+    expect(bigIntToBytes(bigInt)).toEqual(expected);
   });
 });
